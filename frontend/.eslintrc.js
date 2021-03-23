e760e46ecbfe2b5eb34311cdf7fe7f84a28239cf
eunsu27 <pesu1027@gmail.com>
2021-03-23 16:25:51 +0900

    [frontend] 추가 - frontend basic code
    [frontend] 추가 - frontend basic code

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
+  extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/no-use-v-if-with-v-for": "off"
   },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
 };
