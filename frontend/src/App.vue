aeeca06e82b18da00d27afb5e73b30dda2357903
eunsu27 <pesu1027@gmail.com>
2021-03-25 03:26:44 +0900

    [frontend] 수정 - 노래컴포넌트
    노래 컴포넌트 글꼴 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,7 +13,7 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap');
 #app {
-  font-family: 'Nanum Gothic', sans-serif;
+  font-family: 'Nanum Gothic', sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
