b8862dba434a909dcacbaa5867883c3405b9ff71
김주효 <wngytkfkd@naver.com>
2021-04-03 02:18:46 +0900

    [backend] 추가 : 커뮤니티 검색

--- backend/src/main/java/com/ssafy/kpop/service/SearchService.java
+++ backend/src/main/java/com/ssafy/kpop/service/SearchService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.ssafy.kpop.dto.ComPostDto;
+import com.ssafy.kpop.dto.CommunityDto;
 import com.ssafy.kpop.dto.SingerDto;
 import com.ssafy.kpop.dto.SongDto;
 
@@ -11,6 +12,7 @@ public interface SearchService {
 	public List<SingerDto> getSingerByName(String singer_name);
 	public List<SingerDto> getSongByTitle(String song_name);
 	public List<SongDto> getSongBySingerName(String singer_name);
+	public List<CommunityDto> searchCommunity(String c_title);
 	
 	public List<ComPostDto> searchByHash(List<Integer> keywords);
 }
