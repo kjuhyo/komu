5a4e9ba8b6cf304e00c01d040f90f8afe6b5f4ed
김경연 <ghwoer12@naver.com>
2021-04-02 16:47:36 +0900

    [backend] application.properties, kpopapplication

--- backend/src/main/java/com/ssafy/kpop/KpopApplication.java
+++ backend/src/main/java/com/ssafy/kpop/KpopApplication.java
@@ -2,12 +2,17 @@ package com.ssafy.kpop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
+import com.amazonaws.services.elasticmapreduce.model.Application;
 
 @SpringBootApplication
 public class KpopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KpopApplication.class, args);
+		SpringApplication app =  new SpringApplication(KpopApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
 
 }
