1b3e5e0f9fc6529b537224ce76a4fd9024e5e2a9
eunsu27 <pesu1027@gmail.com>
2021-04-06 15:09:11 +0900

    [frontend] 수정 - 노래페이지
    노래페이지 배경이미지 변경

--- frontend/src/views/Song.vue
+++ frontend/src/views/Song.vue
@@ -215,7 +215,7 @@ export default {
   props: {
     header: {
       type: String,
-      default: require('@/assets/img/city-profile.jpg'),
+      default: require('@/assets/img/concert11.jpg'),
     },
   },
   created() {
