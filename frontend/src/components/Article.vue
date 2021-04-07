0135f45c5917dfb945d385e59a32f636e38246eb
eunsu27 <pesu1027@gmail.com>
2021-04-08 01:02:08 +0900

    [frontend] 수정 - 코뮤위키페이지
    css 수정
    검색결과 존재하지 않을 시 문구 띄워주기
    아무것도 검색하지 않을 경우 다시 전체리스트 반환

--- frontend/src/components/Article.vue
+++ frontend/src/components/Article.vue
@@ -1,5 +1,5 @@
 <template>
-  <table>
+  <table class="komuarticle">
     <thead class="thead-css">
       <tr>
         <th width="5%" scope="col">No</th>
@@ -9,31 +9,29 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(komu,idx) in list" :key="idx">
-        <td data-label="No">{{komu.namu_id}}</td>
+      <tr v-for="(komu, idx) in list" :key="idx">
+        <td data-label="No">{{ komu.namu_id }}</td>
         <td data-label="Contents">
           <router-link :to="`/komuwikidetail/${komu.namu_id}`">
-          {{komu.namu_title}}
+            {{ komu.namu_title }}
           </router-link>
-          </td>
-        <td data-label="Writer">{{komu.nickname}}</td>
-        <td data-label="Date">{{komu.namu_date}}</td>
+        </td>
+        <td data-label="Writer">{{ komu.nickname }}</td>
+        <td data-label="Date">{{ komu.namu_date }}</td>
       </tr>
-      </tbody>
+    </tbody>
   </table>
 </template>
 
 <script>
 import '../assets/css/article.css';
 export default {
-  namu:"article",
-  props:{
-    list:{type:Object},
+  namu: 'article',
+  props: {
+    list: { type: Object },
   },
-  data:function(){
-    return{
-    }
-
+  data: function () {
+    return {};
   },
   created() {
     //console.log('리스트');
@@ -44,3 +42,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.komuarticle {
+  padding-bottom: 15px;
+}
+</style>
