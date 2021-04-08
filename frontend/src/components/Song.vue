7f44a1ce8f2ff715bb8594673b491e244be20636
eunsu27 <pesu1027@gmail.com>
2021-04-08 12:47:23 +0900

    [frontend] song.vue 수정
    css 수정
    쓰레기값 삭제

--- frontend/src/components/Song.vue
+++ frontend/src/components/Song.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
     <ul class="cards">
-      <li v-for="(song,idx) in songList" :key="idx" class="cards__item">
+      <li v-for="(song, idx) in songList" :key="idx" class="cards__item">
         <div v-if="song != null" class="song-card">
           <router-link
-          :to="{
-            name: 'songdetail',
-            query: {
-              id: song.id,
-            },
-          }"
-        >
-            <img
-              :src="song.album_cover"
-              class="card__image"
-            />
+            :to="{
+              name: 'songdetail',
+              query: {
+                id: song.id,
+              },
+            }"
+          >
+            <img :src="song.album_cover" class="card__image" />
             <div class="card__content">
-              <div class="card__title">{{ song.song_name }}</div>
+              <div class="r-card__title">{{ song.song_name }}</div>
               <p class="card__text">{{ song.singer_name }}</p>
             </div>
           </router-link>
@@ -30,15 +27,15 @@
 import '../assets/css/song.scss';
 
 export default {
-  name: "song",
-  props:{
-    songList:{type:Object},
+  name: 'song',
+  props: {
+    songList: { type: Object },
   },
   data() {
-    return
+    return;
   },
   created() {
-    console.log(this.songList)
-  }
+    console.log(this.songList);
+  },
 };
 </script>
